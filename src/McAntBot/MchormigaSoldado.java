6d11af62cbf2ea16b1a4ceea6602560475ba71da
Alex <alexisvascot@hotmail.com>
2024-02-28 22:56:19 -0500

    avance pene

--- src/McAntBot/MchormigaSoldado.java
+++ src/McAntBot/MchormigaSoldado.java
@@ -2,11 +2,11 @@ package McAntBot;
 
 import McInteligenciaArtificial.IAbot;
 
-public class MchormigaSoldado extends McAntbot{
+public class MchormigaSoldado {
 
-    public MchormigaSoldado(IAbot iAbot) {
-        super(iAbot);
-        //TODO Auto-generated constructor stub
+    public void mostrar(){
+        System.out.println("La hormiga soldado esta lista");
     }
+
     
 }
